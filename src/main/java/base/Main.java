7356ffb369972349e55ec77dcfe48cdf7fcbd2f3
Far3000-YT <far3000yt@gmail.com>
2025-01-08 18:15:57 +0100

    v1

--- src/main/java/base/Main.java
+++ src/main/java/base/Main.java
@@ -1,14 +1,7 @@
 package base;
 
-import javax.swing.SwingUtilities;
-
 public class Main {
     public static void main(String[] args) {
-        SwingUtilities.invokeLater(() -> {
-            AirlineGUI airlineGUI = new AirlineGUI();
-            airlineGUI.setVisible(true);
-        });
+        System.out.println("TEST");
     }
 }
-
-// to update the data.json file basically close the program
